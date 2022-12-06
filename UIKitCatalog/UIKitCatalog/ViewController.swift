f364b650b1abb8748ea2001806db2607ff29e856
Rabin Joshi <rabin.aapl@gmail.com>
2022-12-06 07:21:23 -0700

    Initial commit.

---
+++ UIKitCatalog/UIKitCatalog/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  UIKitCatalog
+//
+//  Created by Rabin Joshi on 2022-12-06.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            navigateToStackViewController()
+        default:
+            break
+        }
+    }
+
+
+    func navigateToStackViewController() {
+
+
+    }
+}
+
