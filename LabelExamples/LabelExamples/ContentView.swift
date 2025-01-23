6ab19a70f0207b6899996f4331777b931abe1fa5
Rabin Joshi <rabin.aapl@gmail.com>
2025-01-23 13:05:54 -0700

    progress

---
+++ LabelExamples/LabelExamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LabelExamples
+//
+//  Created by Rabin Joshi on 2025-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LabelExamples/LabelExamples/LabelExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelExamplesApp.swift
+//  LabelExamples
+//
+//  Created by Rabin Joshi on 2025-01-23.
+//
+
+import SwiftUI
+
+@main
+struct LabelExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
